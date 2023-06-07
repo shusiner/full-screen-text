f3294a2980a77c018e0bfc5e4c2c0fed370f566a
Ng Huan Ran <quantum.plato@gmail.com>
2023-06-07 23:16:04 +0800

    big word

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,21 @@
-import Link from "next/link"
+"use client"
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import { useState } from "react"
+
+import { Textarea } from "@/components/ui/textarea"
 
 export default function IndexPage() {
+  const [text, setText] = useState("")
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+    <section className="p-4">
+      <Textarea
+        placeholder="Type your message here."
+        onChange={(e) => setText(e.target.value)}
+      />
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+        <h1 className="scroll-m-20 break-all text-7xl font-extrabold tracking-tight">
+          {text}
         </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
       </div>
     </section>
   )
